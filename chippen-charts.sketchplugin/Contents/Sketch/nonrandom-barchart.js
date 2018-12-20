38863cfde597e664b81f70f1fc88574b68b44f1e
monkian <martin.vLupin@web.de>
2018-12-20 14:59:49 +1100

    new build

--- chippen-charts.sketchplugin/Contents/Sketch/nonrandom-barchart.js
+++ chippen-charts.sketchplugin/Contents/Sketch/nonrandom-barchart.js
@@ -86,15 +86,15 @@ var exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/barchart.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/nonrandom-barchart.js");
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ "./src/barchart.js":
-/*!*************************!*\
-  !*** ./src/barchart.js ***!
-  \*************************/
+/***/ "./src/nonrandom-barchart.js":
+/*!***********************************!*\
+  !*** ./src/nonrandom-barchart.js ***!
+  \***********************************/
 /*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -114,11 +114,13 @@ __webpack_require__.r(__webpack_exports__);
 
   var isVertical = isVerticalBarchart(doc.selectedLayers);
   var minMax_fromSelection = getMinMax(doc.selectedLayers, isVertical);
+  var barHeight_fromSelection = getBarHeight(doc.selectedLayers, isVertical);
   /* 
   	User input
   */
 
-  var response = myinput(minMax_fromSelection, doc.selectedLayers.layers.length);
+  var response = myinput(minMax_fromSelection, doc.selectedLayers.layers.length, barHeight_fromSelection);
+  var scaleType_names = ["", "Multiplied by", "Set to max pixel height"];
   var minMax = [response.min, response.max];
 
   if (response.code !== 1000) {
@@ -126,28 +128,32 @@ __webpack_require__.r(__webpack_exports__);
   }
 
   var myRandomSlots = [];
-
-  if (response.trendTypeInput == 0) {
-    // Only for random trend
-    // Set 2 random bars to min and max
-    myRandomSlots = twoRandomSlots(selectedLayers.layers.length, 'Random');
-  }
   /* 
   	UI message
   */
 
-
-  var trendTypeInput_name = ["random data", "trend going up ↑ (linear)", "trend going up ↑ (natural)", "trend going down ↓ (linear)", "trend going down ↓ (natural)"];
   var isVertical_name = "vertical";
 
   if (!isVertical) {
     isVertical_name = "horizontal";
   }
 
-  sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message("".concat(doc.selectedLayers.length, " ").concat(isVertical_name, " bars with ").concat(trendTypeInput_name[response.trendTypeInput], " (").concat(minMax[0], "px\u2013").concat(minMax[1], "px)"));
+  var alert_diff = selectedLayers.layers.length - response.numbers.length;
+  var message_options = "";
+
+  if (response.scaleType != 0) {
+    message_options = "(".concat(scaleType_names[response.scaleType], " ").concat(response.scaleValue, ")");
+  }
+
+  if (alert_diff <= 0) {
+    sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message("\uD83E\uDD19 ".concat(doc.selectedLayers.length, " ").concat(isVertical_name, " bars were adjusted ").concat(message_options));
+  } else {
+    sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message("\uD83E\uDD19 ".concat(response.numbers.length, " ").concat(isVertical_name, " bars were adjusted ").concat(message_options));
+  }
   /* 
   	Changing size
   */
+
 
   for (var i = 0; i < selectedLayers.layers.length; i++) {
     var baseLine = 0;
@@ -157,51 +163,21 @@ __webpack_require__.r(__webpack_exports__);
     } else {// baseline doesn't need correction when changing width in horizontal chart
     }
 
-    var newLength = 1;
+    var newLength = 1; // STAGING
 
-    if (response.trendTypeInput == 3 || response.trendTypeInput == 4) {
-      // reverse minMax if trend is going down
-      // and after that just use same code as for trend going up
-      minMax = [minMax[1], minMax[0]];
-      response.trendTypeInput = response.trendTypeInput - 2;
-    } // Random
-
-
-    if (response.trendTypeInput == 0) {
-      // random values between min and max
-      newLength = Math.floor(Math.random() * (minMax[1] - minMax[0])) + minMax[0];
-
-      if (i == myRandomSlots[0]) {
-        // force a random slot to be min
-        newLength = minMax[0];
+    if (response.numbers[i] == undefined) {
+      newLength = undefined; // no bar will be changed and no error will be thrown (it's undefined by default anyway)
+    } else {
+      if (response.scaleType == 1) {
+        // Multiplier
+        newLength = response.numbers[i] * response.scaleValue;
+      } else if (response.scaleType == 2) {
+        // Set max height
+        newLength = response.numbers[i] * response.scaleValue / response.max;
+      } else {
+        // none
+        newLength = response.numbers[i];
       }
-
-      if (i == myRandomSlots[1]) {
-        // force a random slot to be max
-        newLength = minMax[1];
-      }
-    } // Trend going up
-
-
-    if (response.trendTypeInput == 1 || response.trendTypeInput == 2) {
-      var stepIncrease = (minMax[1] - minMax[0]) / selectedLayers.layers.length;
-
-      if (response.trendTypeInput == 1) {
-        newLength = stepIncrease * i + minMax[0];
-      } else if (response.trendTypeInput == 2) {
-        var increase_random = stepIncrease * Math.random();
-        var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
-        newLength = stepIncrease * i + minMax[0] + increase_random * plusOrMinus;
-      } // force first and last to be min and max
-
-
-      if (i == 0) {
-        newLength = minMax[0];
-      } else if (i == selectedLayers.layers.length - 1) {
-        newLength = minMax[1];
-      }
-
-      newLength = Math.round(newLength * 10) / 10;
     } // Change Width / Height
 
 
@@ -213,6 +189,17 @@ __webpack_require__.r(__webpack_exports__);
     } else {
       // Change Width
       selectedLayers.layers[i].frame.width = newLength;
+    }
+  } // Notification
+  // Alert in case number of selected layers 
+  // does not match amount of numbers
+
+
+  if (alert_diff != 0) {
+    if (alert_diff > 0) {
+      sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.alert("\uD83D\uDE07 Just letting you know", "Only the first ".concat(response.numbers.length, " of your selected layer(s) have been adjusted. There weren't enough number values to adjust the last ").concat(Math.abs(alert_diff), " layer(s). Maybe check the separator options (comma, etc)."));
+    } else if (alert_diff < 0) {
+      sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.alert("\uD83D\uDE07 Just letting you know", "The last ".concat(Math.abs(alert_diff), " values weren't used as there weren't enough layer(s) selected."));
     }
   }
 });
@@ -265,22 +252,17 @@ function getMinMax(arr, isVertical) {
   return [Math.ceil(min), Math.ceil(max)];
 }
 
-function twoRandomSlots(length, type) {
-  var slotOne = 0;
-  var slotTwo = 0;
-
-  if (type == 'Random') {
-    slotOne = Math.floor(Math.random() * length / 2);
-    slotTwo = Math.floor(Math.random() * length / 2) + Math.floor(length / 2);
-  }
-
-  return [slotOne, slotTwo];
-}
-
 function myinput() {
   var myMinMax = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [20, 100];
   var numOfBars = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "";
-  var myresponse = {};
+  var myBarHeightFromSelection = arguments.length > 2 ? arguments[2] : undefined;
+  var myresponse = {
+    code: null,
+    min: null,
+    max: null,
+    numbers: [],
+    trendTypeInput: null
+  };
 
   if (myMinMax.length != 2 || myMinMax[0] == myMinMax[1]) {
     if (myMinMax[0] > 100) {
@@ -301,89 +283,122 @@ function myinput() {
     } // Create initial view panel
 
 
-    var width = 280;
-    var height = 40;
-    var view = NSView.alloc().initWithFrame(NSMakeRect(0, 0, width, height)); // Min Max input
+    var width = 260;
+    var height = 40; // Number input
 
-    var minInput = NSTextField.alloc().initWithFrame(NSMakeRect(0.0, 15.0, 130, 25.0));
-    minInput.cell().setPlaceholderString(myMinMax[0] + " (min)");
-    view.addSubview(minInput);
-    var maxInput = NSTextField.alloc().initWithFrame(NSMakeRect(150, 15.0, 130, 25.0));
-    maxInput.cell().setPlaceholderString(myMinMax[1] + " (max)");
-    view.addSubview(maxInput); // Nature of data
-
-    var trendTypeInput = NSView.alloc().initWithFrame(NSMakeRect(0, 0, width, 125)); // Acts like a template (prototype) for our radio buttons
-
-    var buttonFormat;
-    buttonFormat = NSButtonCell.alloc().init();
-    buttonFormat.setButtonType(NSRadioButton); // The matrix will contain all the cells (radio buttons)
-
-    var matrixFormat = NSMatrix.alloc().initWithFrame_mode_prototype_numberOfRows_numberOfColumns(NSMakeRect(0, 0, 260, 125), // Horizontal position, vertical position, width, height
-    NSRadioModeMatrix, // This makes the radio buttons work together
-    buttonFormat, 5, // 1 row
-    1 // 3 columns (for 3 radio buttons)
-    ); // Settings the size of the radio buttons
-
-    matrixFormat.setCellSize(CGSizeMake(260, 25)); // Adding the radio buttons to the form
-
-    var cells = matrixFormat.cells();
-    cells.objectAtIndex(0).setTitle("Random");
-    cells.objectAtIndex(1).setTitle("Trend going up ↑ (linear)");
-    cells.objectAtIndex(2).setTitle("Trend going up ↑ (natural)");
-    cells.objectAtIndex(3).setTitle("Trend going down ↓ (linear)");
-    cells.objectAtIndex(4).setTitle("Trend going down ↓ (natural)"); // Adding the matrix to the form
-
-    trendTypeInput.addSubview(matrixFormat); // Setup the window
+    var sampleNumbers = myBarHeightFromSelection;
+    /*
+    	Setup the window
+    	*/
 
     var alert = COSAlertWindow.new();
 
     if (numOfBars == 1) {
-      alert.setMessageText("Create your random bar chart \nwith 1 selected layer");
+      alert.setMessageText("Create your defined bar chart \nwith 1 selected layer");
     } else {
-      alert.setMessageText("Create your random bar chart \nwith ".concat(numOfBars, " layers selected"));
+      alert.setMessageText("Create your defined bar chart \nwith ".concat(numOfBars, " selected layers"));
     }
 
     alert.addButtonWithTitle("Run");
     alert.addButtonWithTitle("Cancel");
-    alert.setInformativeText("This is for a bar chart where random values are applied \nto the layers of your selection.");
-    var extrema_label = createLabel("Define extrema of bars", 12, true, NSMakeRect(0, 0, width, 16));
-    alert.addAccessoryView(extrema_label); //alert.addTextLabelWithValue("Define extrema of bars");
+    alert.setInformativeText("This is for a bar chart where you have specific values in mind, rather than using random numbers.");
+    /*
+    	Input
+    	*/
 
-    alert.alert().window().setInitialFirstResponder(minInput);
-    alert.addAccessoryView(view);
-    var trend_label = createLabel("Specify the desired trend", 12, true, NSMakeRect(0, 0, width, 16));
-    alert.addAccessoryView(trend_label); //alert.addTextLabelWithValue("Specify the desired trend");
+    var alert_width = 280;
+    var numInput_separatorOptions = ["Comma separated", "Space separated", "Tab separated (Excel row)", "Line break separated (Excel column)"];
+    var numInput_label = createLabel("Paste in your number values", 12, true, NSMakeRect(0, 0, alert_width, 16));
+    var numInput = createTextField(sampleNumbers, null, NSMakeRect(0, 0, alert_width, 25));
+    var numInput_separator = createDropdown([numInput_separatorOptions[0], numInput_separatorOptions[1], numInput_separatorOptions[2], numInput_separatorOptions[3]], NSMakeRect(-2, -1, 150, 24));
+    alert.addAccessoryView(numInput_label);
+    alert.addAccessoryView(numInput);
+    alert.addAccessoryView(numInput_separator);
+    /*
+    Options
+    */
 
-    alert.addAccessoryView(trendTypeInput);
+    var optionsView_height = 85;
+    var optionsView = NSView.alloc().initWithFrame(NSMakeRect(0, 0, alert_width, optionsView_height));
+    var optionsLabel = createLabel("Do you want to scale these values?", 12, true, NSMakeRect(0, optionsView_height - 26, alert_width, 16));
+    var optionsLabel_inlineNote = createLabel("(optional)", 12, false, NSMakeRect(215, optionsView_height - 26, alert_width, 16), 0.3);
+    var option1_label = createLabel("Multiplier", 12, false, NSMakeRect(0, optionsView_height - 48, 130, 16));
+    var option1_textField = createTextField("", "e.g. 1", NSMakeRect(0, optionsView_height - 81, 130, 25));
+    var option2_label = createLabel("Max bar height (px)", 12, false, NSMakeRect(150, optionsView_height - 48, 130, 16));
+    var option2_textField = createTextField("", "e.g. " + myMinMax[1], NSMakeRect(150, optionsView_height - 81, 130, 25));
+    var options_info = createLabel("You have the option to define the scaling in case the supplied values don't match your desired pixel values. You can either define a multiplier or set a maximum bar height in pixel.", 11, false, NSMakeRect(0, 0, 260, 16 * 4));
+    optionsView.addSubview(optionsLabel);
+    optionsView.addSubview(optionsLabel_inlineNote);
+    optionsView.addSubview(options_info);
+    optionsView.addSubview(option1_label);
+    optionsView.addSubview(option1_textField);
+    optionsView.addSubview(option2_label);
+    optionsView.addSubview(option2_textField);
+    alert.addAccessoryView(optionsView);
+    alert.addAccessoryView(options_info);
     /*
     Note
     */
 
-    var note_line1 = createLabel("\nPlease make sure proportional scaling is disabled", 11, false, NSMakeRect(0, 0, width, 16 * 2), 0.3);
+    var note_line1 = createLabel("Please make sure proportional scaling is disabled", 11, false, NSMakeRect(0, 0, alert_width + 10, 16), 0.3);
     alert.addAccessoryView(note_line1);
+    /*
+    RESPONSE
+    */
+
     var responseCode = alert.runModal();
 
     if (responseCode == 1000) {
       myresponse.code = 1000; // OK
-      // pass minmax input
+      // Separator
 
-      if (minInput.stringValue() == "") {
-        myresponse.min = myMinMax[0];
-      } else {
-        myresponse.min = parseInt(minInput.stringValue());
+      var sep = ",";
+      var sep_input = numInput_separator.titleOfSelectedItem();
+
+      if (sep_input == numInput_separatorOptions[1]) {
+        sep = " ";
       }
 
-      if (maxInput.stringValue() == "") {
-        myresponse.max = myMinMax[1];
+      if (sep_input == numInput_separatorOptions[2]) {
+        sep = "\t";
+      } else if (sep_input == numInput_separatorOptions[3]) {
+        sep = "\n";
+      } // Numbers
+
+
+      var numbers_str = "";
+
+      if (numInput.stringValue() == "") {
+        numbers_str = sampleNumbers;
       } else {
-        myresponse.max = parseInt(maxInput.stringValue());
+        numbers_str = numInput.stringValue();
+      }
+
+      var numbers_arr = numbers_str.split(sep);
+
+      for (var i = 0; i < numbers_arr.length; i++) {
+        myresponse.numbers.push(parseFloat(numbers_arr[i]));
+      } // Options
+
+
+      if (option2_textField.stringValue() != "") {
+        // Option 2: Set max height
+        myresponse.scaleType = 2;
+        myresponse.scaleValue = parseFloat(option2_textField.stringValue());
+      } else if (option1_textField.stringValue() != "") {
+        // Option 1: Multiplier
+        myresponse.scaleType = 1;
+        myresponse.scaleValue = parseFloat(option1_textField.stringValue());
+      } else {
+        myresponse.scaleType = 0;
       }
     } else {
       // Cancel
       myresponse.code = 1001;
     }
 
-    myresponse.trendTypeInput = matrixFormat.cells().indexOfObject(matrixFormat.selectedCell());
+    myresponse.min = myMinMax[0];
+    myresponse.max = myMinMax[1];
   }
 
   return myresponse;
@@ -400,6 +415,31 @@ function isVerticalBarchart(arr) {
   }
 
   return vertical;
+}
+
+function getBarHeight(arr, isVertical) {
+  // arr needs to be doc.selectedLayers
+  var barLength_str = "";
+
+  for (var i = 0; i < arr.layers.length; i++) {
+    var thisLength = undefined;
+
+    if (isVertical) {
+      thisLength = arr.layers[i].frame.height.toFixed(2); // reduce to 2 decimals
+    } else {
+      thisLength = arr.layers[i].frame.width.toFixed(2); // reduce to 2 decimals
+    }
+
+    thisLength = parseFloat(thisLength); // to remove decimals from integers
+
+    if (i == 0) {
+      barLength_str = "" + thisLength;
+    } else {
+      barLength_str = barLength_str + "," + thisLength;
+    }
+  }
+
+  return barLength_str;
 }
 /*
 	Utils from Marc Bouchenoire
@@ -457,4 +497,4 @@ module.exports = require("sketch");
 }
 that['onRun'] = __skpm_run.bind(this, 'default')
 
-//# sourceMappingURL=barchart.js.map
+//# sourceMappingURL=nonrandom-barchart.js.map
